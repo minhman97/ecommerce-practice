97e8afd8aca6fc4d96cb15a613566ba8f9521dca
Man Nguyen <minhman.carter@gmail.com>
2023-03-25 15:06:55 +0700

    init banner

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import React from 'react'
+import {urlFor} from '../lib/client'
+
+const HeroBanner = ({heroBanner}) => {
+  console.log(heroBanner.image)
+  return (
+    <div className='hero-banner-container'>
+      <div>
+        <p className="beats-solo">{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{`Fine`}</h1>
+        <img src={urlFor(heroBanner.image[0])} alt="headphones" className='hero-banner-image'/>
+        <div>
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type='button'>{heroBanner.buttonText}</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>{heroBanner.desc}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HeroBanner
